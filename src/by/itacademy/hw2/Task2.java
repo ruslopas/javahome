409a8297ac5f3e27fc4ead1d247356a484d24b5a
ruslopas <ruslopas@gmail.com>
2022-08-04 13:03:50 +0300

    corrected name sumAndMult -> sumAndMultiply

--- src/by/itacademy/hw2/Task2.java
+++ src/by/itacademy/hw2/Task2.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 
 public class Task2 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int a, b, c;
+        int a, b, c;
 
-		System.out.println("Введите первое число (а)");
-		Scanner scanner = new Scanner(System.in);
-		a = scanner.nextInt();
+        System.out.println("Введите первое число (а)");
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
 
-		System.out.println("Введите второе число (b)");
-		b = scanner.nextInt();
-		scanner.close();
+        System.out.println("Введите второе число (b)");
+        b = scanner.nextInt();
+        scanner.close();
 
-		c = sumAndMult(a, b);
+        c = sumAndMultiply(a, b);
 
-		System.out.println("(a + b) * (a * b) = " + c);
-	}
+        System.out.println("(a + b) * (a * b) = " + c);
+    }
 
-	public static int sumAndMult(int a, int b) {
+    public static int sumAndMultiply(int a, int b) {
 
-		return (a + b) + a * b;
-	}
+        return (a + b) + a * b;
+    }
 }
