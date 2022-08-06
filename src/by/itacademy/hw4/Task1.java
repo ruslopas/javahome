5d6f72c3f423c3b07443efd7058de160d39033ee
ruslopas <ruslopas@gmail.com>
2022-08-06 20:10:16 +0300

    addede Task1

--- src/by/itacademy/hw4/Task1.java
+++ src/by/itacademy/hw4/Task1.java
@@ -7,7 +7,20 @@ package by.itacademy.hw4;
 public class Task1 {
 
 	public static void main(String[] args) {
-		
+
+		int[][] myArray = new int[11][11];
+
+		for (int i = 1; i < myArray.length; i++) {
+
+			for (int j = 1; j < myArray.length; j++) {
+				
+				myArray[i][j] = i * j;
+
+				System.out.print(myArray[i][j] + " ");
+			}
+			
+			System.out.println(); 
+		}
 
 	}
 
