0a35eb60041a95657f5cca31c7e149314f3744d9
ruslopas <ruslopas@gmail.com>
2022-08-07 17:31:45 +0300

    added Task7

--- src/by/itacademy/hw4/Task7.java
+++ src/by/itacademy/hw4/Task7.java
@@ -1,4 +1,5 @@
 package by.itacademy.hw4;
+
 /*
  * Создать массив заполнить его случайными элементами, распечатать,
  * перевернуть, и снова распечатать
@@ -6,7 +7,28 @@ package by.itacademy.hw4;
 public class Task7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int arrayIndex = 10;
+		int[] myArray = new int[arrayIndex];
+		System.out.println("Created array");
+
+		for (int i = 0; i < arrayIndex; i++) {
+			myArray[i] = (int) (Math.random() * 100 + 1);
+			System.out.print(" " + myArray[i]);
+		}
+		for (int i = 0; i < arrayIndex / 2; i++) {
+			int exchange = myArray[i];
+			myArray[i] = myArray[arrayIndex - 1 - i];
+			myArray[arrayIndex - 1 - i] = exchange;
+
+		}
+		
+		System.out.println();
+		System.out.println("Reversed array");
+
+		for (int i = 0; i < arrayIndex; i++) {
+			System.out.print(" " + myArray[i]);
+		}
 
 	}
 
