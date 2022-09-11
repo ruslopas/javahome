ce5339f97b9e8ea70f650dd731c00874bce3d7e7
ruslopas <ruslopas@gmail.com>
2022-09-11 12:02:33 +0300

    corrected task4

--- src/by/itacademy/hw12/task4/Main.java
+++ src/by/itacademy/hw12/task4/Main.java
@@ -24,9 +24,10 @@ public class Main {
 	File persons = new File("persons.txt");
 
 	try (FileOutputStream personsOut = new FileOutputStream(persons)) {
-	    System.out.println("Create file...");
+	    System.out.println("Create file...\n");
 
 	    ObjectOutputStream output = new ObjectOutputStream(personsOut);
+	    
 	    output.writeObject(vasya);
 	    output.writeObject(petya);
 	    output.writeObject(vika);
@@ -34,48 +35,49 @@ public class Main {
 	    output.writeObject(dima);
 
 	} catch (FileNotFoundException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
+	    System.out.println(e.getMessage());
+
 	} catch (IOException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
+	    System.out.println(e.getMessage());
+
 	}
 	if (persons.exists()) {
 	    System.out.printf("File \"%s\" is available on the path -> %s\n", persons.getName(),
 		    persons.getAbsolutePath());
 	}
+	
 	List<Person> personsArray = new ArrayList<>();
 
 	try (FileInputStream personsIn = new FileInputStream(persons)) {
 	    ObjectInputStream input = new ObjectInputStream(personsIn);
-	
+
 	    Person rest1 = (Person) input.readObject();
 	    Person rest2 = (Person) input.readObject();
 	    Person rest3 = (Person) input.readObject();
 	    Person rest4 = (Person) input.readObject();
 	    Person rest5 = (Person) input.readObject();
-	    
+
 	    personsArray.add(rest1);
 	    personsArray.add(rest2);
 	    personsArray.add(rest3);
 	    personsArray.add(rest4);
 	    personsArray.add(rest5);
 
-	    System.out.printf("%s %s %s\n",rest1.getName(),rest1.getSurname(),rest1.getAge());
-	    System.out.printf("%s %s %s\n",rest2.getName(),rest2.getSurname(),rest2.getAge());
-	    System.out.printf("%s %s %s\n",rest3.getName(),rest3.getSurname(),rest3.getAge());
-	    System.out.printf("%s %s %s\n",rest4.getName(),rest4.getSurname(),rest4.getAge());
-	    System.out.printf("%s %s %s\n",rest5.getName(),rest5.getSurname(),rest5.getAge());
+	    System.out.printf("\n%s %s %s\n", rest1.getName(), rest1.getSurname(), rest1.getAge());
+	    System.out.printf("%s %s %s\n", rest2.getName(), rest2.getSurname(), rest2.getAge());
+	    System.out.printf("%s %s %s\n", rest3.getName(), rest3.getSurname(), rest3.getAge());
+	    System.out.printf("%s %s %s\n", rest4.getName(), rest4.getSurname(), rest4.getAge());
+	    System.out.printf("%s %s %s\n", rest5.getName(), rest5.getSurname(), rest5.getAge());
 
 	} catch (FileNotFoundException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
+	    System.out.println(e.getMessage());
+	
 	} catch (IOException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
+	   System.out.println(e.getMessage());
+	
 	} catch (ClassNotFoundException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
+	    System.out.println(e.getMessage());
+	  
 	}
 
 	System.out.println();
