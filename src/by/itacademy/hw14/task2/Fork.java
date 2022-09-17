bab6cfd4035c695597922e51dab60ef8052dbe80
ruslopas <ruslopas@gmail.com>
2022-09-17 21:04:32 +0300

    added task2

---
+++ src/by/itacademy/hw14/task2/Fork.java
@@ -0,0 +1,31 @@
+package by.itacademy.hw14.task2;
+
+public class Fork {
+    
+    private static int count = 0;
+    private int id;
+    private boolean isBusy;
+
+    public Fork() {
+	id = ++count;
+	isBusy = false;
+    }
+
+    public synchronized void take() {
+	if (isBusy) {
+	    try {
+		wait();
+	    } catch (InterruptedException e) {
+		e.printStackTrace();
+	    }
+	}
+	System.out.println(Thread.currentThread().getName() + " схватил вилку " + id);
+	isBusy = true;
+    }
+
+    public synchronized void drop() {
+	System.out.println(Thread.currentThread().getName() + " бросил вилку " + id);
+	isBusy = false;
+	notify();
+    }
+}
