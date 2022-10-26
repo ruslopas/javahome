40dbf2944f2c847e3efdd2d9fb9b057a6ddff6aa
ruslopas <ruslopas@gmail.com>
2022-10-26 18:25:27 +0300

    hw4 task1

--- src/by/itacademy/hw4/Task1.java
+++ src/by/itacademy/hw4/Task1.java
@@ -1,30 +1,26 @@
 package by.itacademy.hw4;
 
-
 /*
 Необходимо вывести на экран таблицу умножения от 1 до 10.
 */
 
 public class Task1 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.out.println("Multiplication table");
+	System.out.println("Multiplication table");
 
-		int[][] myArray = new int[11][11];
+	int[][] myArray = new int[11][11];
 
-		for (int i = 1; i < myArray.length; i++) {
+	for (int i = 0; i < myArray.length; i++) {
+	    for (int j = 0; j < myArray.length; j++) {
+		myArray[i][j] = i * j;
+		System.out.print(myArray[i][j] + " ");
+	    }
 
-			for (int j = 1; j < myArray.length; j++) {
-
-				myArray[i][j] = i * j;
-
-				System.out.print(myArray[i][j] + " ");
-			}
-
-			System.out.println();
-		}
-
+	    System.out.println();
 	}
+
+    }
 
 }
